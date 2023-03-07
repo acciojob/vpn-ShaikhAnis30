1f04a899f0b20822bc701276d4286fd0c72a1bfd
ShaikhAnis30 <anisshaikh8535@gmail.com>
2023-03-07 23:38:40 +0530

    User logic changed

--- src/main/java/com/driver/services/impl/AdminServiceImpl.java
+++ src/main/java/com/driver/services/impl/AdminServiceImpl.java
@@ -74,41 +74,7 @@ public class AdminServiceImpl implements AdminService {
         serviceProvider.setCountryList(countryList);
 
         serviceProviderRepository1.save(serviceProvider);
-//        countryRepository1.save(country);
+        //countryRepository1.save(country);
         return serviceProvider;
-//        if(countryName.equalsIgnoreCase("IND") || countryName.equalsIgnoreCase("USA") || countryName.equalsIgnoreCase("JPN") || countryName.equalsIgnoreCase("CHI") || countryName.equalsIgnoreCase("AUS")){
-//            Country country = new Country();
-//
-//            ServiceProvider serviceProvider = serviceProviderRepository1.findById(serviceProviderId).get();
-//
-//            if(countryName.equalsIgnoreCase("IND")){
-//                country.setCountryName(CountryName.IND);
-//                country.setCode(CountryName.IND.toCode());
-//            }
-//            if(countryName.equalsIgnoreCase("USA")){
-//                country.setCountryName(CountryName.USA);
-//                country.setCode(CountryName.USA.toCode());
-//            }
-//            if(countryName.equalsIgnoreCase("JPN")){
-//                country.setCountryName(CountryName.JPN);
-//                country.setCode(CountryName.JPN.toCode());
-//            }
-//            if(countryName.equalsIgnoreCase("CHI")){
-//                country.setCountryName(CountryName.CHI);
-//                country.setCode(CountryName.CHI.toCode());
-//            }
-//            if(countryName.equalsIgnoreCase("AUS")){
-//                country.setCountryName(CountryName.AUS);
-//                country.setCode(CountryName.AUS.toCode());
-//            }
-//            country.setServiceProvider(serviceProvider);
-//            serviceProvider.getCountryList().add(country);
-//            serviceProviderRepository1.save(serviceProvider);
-//
-//            return serviceProvider;
-//        }
-//        else{
-//            throw new Exception("Country not found");
-//        }
     }
 }
